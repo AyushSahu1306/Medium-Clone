8a1777e8c6d38bfde5180dfd3878b691fe95ed67
Ayush Sahu <sahuayush0613@gmail.com>
2025-08-03 15:30:50 +0530

    Initial project setup with Convex and Clerk

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,9 +2,11 @@
 
 import { ThemeProvider, useTheme } from 'next-themes'
 import { Toaster } from '@/components/ui/sonner'
+import {ConvexClientProvider} from '@/components/convex-client-provider'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
+    <ConvexClientProvider>
     <ThemeProvider
       enableSystem
       attribute='class'
@@ -14,6 +16,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       {children}
       <ToasterProvider />
     </ThemeProvider>
+    </ConvexClientProvider>
   )
 }
 
